857561bb25c12c7bed917dc983b4ee4eba7416e5
Bishnu Sunar <bishnusunar@Bishnus-MacBook-Pro.local>
2026-05-16 20:47:49 +0545

    removed language from plugin hed

--- wise-widget-manager.php
+++ wise-widget-manager.php
@@ -9,7 +9,6 @@
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       wise-widget-manager
- * Domain Path:       /languages
  * Requires at least: 6.0
  * Requires PHP:      7.4
  *
